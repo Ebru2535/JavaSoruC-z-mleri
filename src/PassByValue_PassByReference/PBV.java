3faa148d27d30fa1f610bf2e326bc350a9e23f40
ebrusavas.2015@gmail.com <ebrusavas.2015@gmail.com>
2023-04-15 00:21:01 +0300

    15/04/2023

--- src/PassByValue_PassByReference/PBV.java
+++ src/PassByValue_PassByReference/PBV.java
@@ -8,14 +8,24 @@ public class PBV {
         List<Integer> liste=new ArrayList<>();
         liste.add(10);
         liste.add(20);
+        System.out.println(listeyiYeniDegerAta(liste));  //  [30, 40]
+        System.out.println(liste); // [10, 20]
+
+        System.out.println(listeElementleriniDegistir(liste));  // [50, 60]
+        System.out.println(liste);      // [50, 60]
 
 
-    } public static List<Integer>ListeyiYeniDegerAta(List<Integer>liste){
+    } public static List<Integer>listeyiYeniDegerAta(List<Integer>liste){
         List<Integer>yeniListe=new ArrayList<>();
         yeniListe.add(30);
         yeniListe.add(40);
         liste=yeniListe;
         return liste;
-        // sayfa no 46
+
+    } public static List<Integer> listeElementleriniDegistir(List<Integer> liste){
+        liste.set(0,50);
+        liste.set(1,60);
+        return liste;
     }
+
 }
---
+++ src/PassByValue_PassByReference/PSV1.java
@@ -0,0 +1,29 @@
+package PassByValue_PassByReference;
+
+import java.util.Arrays;
+
+public class PSV1 {
+    public static void main(String[] args) {
+        int [] arr={10,20,30};
+        System.out.println(Arrays.toString(arrayeYeniDegerAta(arr))); // [40, 50, 60]
+        System.out.println(Arrays.toString(arr));  // [10, 20, 30]   java byPasvalue yu kullanır.
+
+
+        System.out.println(Arrays.toString(arrayElementDegistir(arr))); //[80, 90, 30]
+
+        System.out.println(Arrays.toString(arrayElementDegistir(arr)));//[80, 90, 30]
+        // elementleri değiştirdiğimizde objenın referansı değişmediği için elemntlerin değişikliği kalıcı olur
+    }
+
+
+         public static int[] arrayeYeniDegerAta(int [] arr) {
+           int [] yeniarr={40,50,60};
+           arr=yeniarr;
+           return arr;
+    }
+    public static int[] arrayElementDegistir(int[]arr){
+        arr[0]=80;
+        arr[1]=90;
+        return arr;
+    }
+}
