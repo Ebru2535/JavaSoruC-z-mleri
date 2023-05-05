a86d4a32b3f4bc5857dd44764f93e7e0933a8cee
Ebru2535 <ebrusavas.2015@gmail.com>
2023-05-05 22:56:47 +0300

    05/05/2023

---
+++ src/Interface/CanFly.java
@@ -0,0 +1,13 @@
+package Interface;
+
+public interface CanFly {
+    void fly();
+
+}
+interface HasWings {
+    public abstract Object getWindSpan();
+
+}
+abstract class Falcon implements CanFly,HasWings {
+
+}
